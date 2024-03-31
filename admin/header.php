5e8eb402c464bf8d10c9cc380a9aaa83ae23e5fd
louremipsum <myself.vinayak2@gmail.com>
2024-03-31 19:54:07 +0530

    style: tabs navbar in laptop screen and burger in mobile screen

--- admin/header.php
+++ admin/header.php
@@ -1,8 +1,6 @@
 <?php
 include'lib/connection.php';
-// $sql = "SELECT * FROM orders where status='pending'";
-// $result = $conn -> query ($sql);
-
+$current_page = basename($_SERVER['PHP_SELF']);
 ?>
 
 <!DOCTYPE html>
@@ -17,36 +15,80 @@ include'lib/connection.php';
         integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous" />
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/media.css">
+    <style>
+    .nav-link {
+        font-weight: 500;
+    }
+
+    .nav-link:hover {
+        color: darkblue !important;
+        font-weight: 500 !important;
+    }
+    </style>
 </head>
 
 <body>
     <section class="header d-flex justify-content-between align-items-center" id="header">
         <div class="d-flex align-items-center">
-            <i class="fas fa-bars fixed" onclick="openside()"></i>
-            <div class="line-fixed">Admin Panel</div>
+            <i class="fas fa-bars fixed d-sm-none" onclick="openside()"></i>
+            <div class="line-fixed ms-5">Admin Panel</div>
         </div>
         <a href="logout.php" class="btn btn-danger me-5">Logout</a>
     </section>
 
-    <div class="sidenav" id="sidenav">
-        <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link d" href="Home.php">Dashboard</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link po" href="transaction.php">Transaction</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link ap" href="add_product.php">Add Product</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link vp" href="all_product.php">All Product</a>
-            </li>
+    <div class="d-block d-sm-none">
+        <div class="sidenav" id="sidenav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link d" href="Home.php">Dashboard</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link po" href="transaction.php">Transaction</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link ao" href="confirm_transaction.php">Confirm Transaction</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link ap" href="add_product.php">Add Product</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link vp" href="all_product.php">All Product</a>
+                </li>
 
-            <li class="nav-item">
-                <a class="nav-link ao" href="all_orders.php">Order</a>
+                <li class="nav-item">
+                    <a class="nav-link ao" href="all_orders.php">Order</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="d-none d-sm-block ms-5">
+        <ul class="nav nav-pills mb-3" id="myTab" role="tablist">
+            <li class="nav-item" role="presentation">
+                <a class="nav-link <?php echo ($current_page == 'Home.php') ? 'active' : ''; ?>"
+                    href="Home.php">Dashboard</a>
+            </li>
+            <li class="nav-item" role="presentation">
+                <a class="nav-link <?php echo ($current_page == 'transaction.php') ? 'active' : ''; ?>"
+                    href="transaction.php">Transaction</a>
+            </li>
+            <li class="nav-item" role="presentation">
+                <a class="nav-link <?php echo ($current_page == 'confirm_transaction.php') ? 'active' : ''; ?>"
+                    href="confirm_transaction.php">Confirm Transaction</a>
+            </li>
+            <li class="nav-item" role="presentation">
+                <a class="nav-link <?php echo ($current_page == 'add_product.php') ? 'active' : ''; ?>"
+                    href="add_product.php">Add Product</a>
+            </li>
+            <li class="nav-item" role="presentation">
+                <a class="nav-link <?php echo ($current_page == 'all_product.php') ? 'active' : ''; ?>"
+                    href="all_product.php">All Product</a>
+            </li>
+            <li class="nav-item" role="presentation">
+                <a class="nav-link <?php echo ($current_page == 'all_orders.php') ? 'active' : ''; ?>"
+                    href="all_orders.php">Order</a>
             </li>
         </ul>
+
     </div>
     <?php
 
