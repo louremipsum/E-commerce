f1edec8ae63c98988c6351581b70292459d5d2d9
louremipsum <myself.vinayak2@gmail.com>
2024-03-31 19:54:42 +0530

    fix: session_start should be before any html output which was not the case header was included before session starts

--- admin/home.php
+++ admin/home.php
@@ -1,6 +1,6 @@
 <?php
- include'header.php';
  SESSION_START();
+ include'header.php';
 
 if(isset($_SESSION['auth']))
 {
@@ -17,6 +17,7 @@ include'lib/connection.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -25,17 +26,19 @@ include'lib/connection.php';
     <link rel="stylesheet" href="css/home.css">
 
 </head>
+
 <body>
-    
+
     <div class="container homebody">
         <div class="row">
             <div class="col-md-12">
                 <h1>Welcome To The Admin Panel</h1>
-                
+
 
             </div>
         </div>
     </div>
-    
+
 </body>
+
 </html>
