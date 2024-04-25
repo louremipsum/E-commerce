98f4e23e00800b70228b0610bca27cae6f3b7363
Samarth <samarthpatil2021.comp@mmcoe.edu.in>
2024-04-25 23:32:25 +0530

    lib added

---
+++ admin/lib/connection.php
@@ -0,0 +1,19 @@
+<?php 
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db   ="wtProject";
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn -> connect_error) 
+{
+	die($conn -> error);
+}
+else
+{
+	//echo "database connected";
+}
+
+?>
